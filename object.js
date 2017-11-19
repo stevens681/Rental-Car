d2828f9e7dba0f4f7027628ad81304491b247287
Fernando Rosa <stevensfrr@gmail.com>
2017-11-19 00:35:56 +0000

    Second Car app

--- object.js
+++ object.js
@@ -1,50 +1,87 @@
-
-
-
 var rentals ={
-    name: 'Enterprise Rental',
-    econCars: 500,
+    name: "Enterprise Rental",
+    econCars: 10,
     ebooked: 0,
     econType: "Economy",
-    midCars: 500,
+    midCars: 10,
     mbooked: 0,
     midType: "Midsize",
+    renter:[ {rName: "",
+        brand:""
+    }],
     
     ebook: function() {
         this.ebooked++;
-        rentals.carsAvail();
+        if(this.econCars > 0){
+            this.econCars--;
+            rentals.carsAvail();
+        }else{
+            alert("We ran out of Economy Cars")
+        }
+        
     },
     mbook: function() {
         this.mbooked++;
-        rentals.carsAvail();
+        if(this.midCars > 0){
+            this.midCars--;
+            rentals.carsAvail();
+        }else{
+            alert("We ran out of Mid size Cars")
+        }
+        
     },
     carsAvail: function() {
-     document.getElementById("ecarAvail").innerHTML = rentals.econCars - rentals.ebooked;
-     document.getElementById('mcarAvail').innerHTML = rentals.midCars -  rentals.mbooked;
+    
+    
+     document.getElementById("ecarAvail").innerHTML = rentals.econCars;
+     document.getElementById('mcarAvail').innerHTML = rentals.midCars;
+    },
+   addRenter: function(renterName, carBrand) {
+        this.renter.push({
+            rName: renterName,
+            brand: carBrand,
+        });
     }
 };
 
+
 document.getElementById('upbutton').onclick = function() {
     document.getElementById('businessname').innerHTML = rentals.name;
-    document.getElementById('economycar').innerHTML = rentals.econType
-    document.getElementById('midcar').innerHTML = rentals.midType;
     rentals.carsAvail();
     
 };
 
-document.getElementById('econbutton').onclick = function() {
-    rentals.ebook();
+
+function dropDown(){
+   var selection = document.getElementById("carDropdown").value;
+        if(selection === "econbutton"){
+            document.getElementById('economycar').innerHTML = rentals.econType;
+            
+        }else if(selection === "midbutton"){
+            document.getElementById('midcar').innerHTML = rentals.midType;
+        }
 }
 
-document.getElementById('midbutton').onclick = function() {
-    rentals.mbook();
+function rentCar(){
+       var rent = document.getElementById("carDropdown").value;
+       var name = document.getElementById("rName").value;
+       var brand = document.getElementById("carBrand").value;
+       if(name != ""){
+        if(rent === "econbutton"){
+            rentals.ebook();
+            rentals.addRenter(name, brand);
+            document.getElementById('economycar').innerHTML = rentals.econType;
+            document.getElementById('renterName').innerHTML = name;
+            
+        }else if(rent === "midbutton"){
+            rentals.mbook();
+            rentals.addRenter(name, brand);
+            document.getElementById('midcar').innerHTML = rentals.midType;
+            document.getElementById('renterName').innerHTML = name;
+            
+        }
+
+       }else{
+           alert("Please enter the customer name");
+       }
 }
-
-
-
-
-
-
-
-
-
